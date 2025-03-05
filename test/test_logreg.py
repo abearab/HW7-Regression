469c3eade1194b4b247aab2f261224c9d233d1fb
Abe Arab <abarbiology@gmail.com>
2025-03-04 23:11:27 -0800

    implement `test_loss_function`

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -15,11 +15,13 @@ from regression import (logreg, utils)
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
 
 # (you will probably need to import more things here)
 
 
 def test_prediction():
+	# Load data
 	X_train, X_val, y_train, y_val = _load_data()
     # Scale the data, since values vary across feature. Note that we
     # fit on the training data and use the same scaler for X_val.
@@ -49,7 +51,31 @@ def test_prediction():
 
 
 def test_loss_function():
-	pass
+	# Load data
+	X_train, X_val, y_train, y_val = _load_data()
+	# Scale the data, since values vary across feature. Note that we
+	# fit on the training data and use the same scaler for X_val.
+	sc = StandardScaler()
+	X_train = sc.fit_transform(X_train)
+	X_val = sc.transform(X_val)
+
+	# Create a logistic regression model from sklearn
+	sklearn_model = LogisticRegression(solver='saga',max_iter=1000,random_state=42)
+	sklearn_model.fit(X_train, y_train)
+	sklearn_pred = sklearn_model.predict(X_val)
+	sklearn_loss = log_loss(y_val, sklearn_pred)
+
+	# Create a logistic regression model from scratch
+	log_model = logreg.LogisticRegressor(num_feats=X_train.shape[1], max_iter=1000, learning_rate=0.1, batch_size=1000)
+	log_model.train_model(X_train, y_train, X_val, y_val)
+	log_model_pred = log_model.make_prediction(X_val)
+	# convert the predicted values to 0 or 1
+	log_model_pred = np.where(log_model_pred > 0.5, 1, 0)
+	log_model_loss = log_model.loss_function(y_val, log_model_pred)
+
+	# Check that the loss of the two models is similar
+	assert np.isclose(sklearn_loss, log_model_loss, atol=0.5), \
+	"loss of the implemented model is not sufficiently close to the sklearn model"
 
 
 def test_gradient():
